a7946418cb204859f3737b3e60007c70b9826c4f
sanjaipandi66@gmail.com <sanjaipandi66@gmail.com>
2025-02-21 15:16:59 +0530

    done

--- index.js
+++ index.js
@@ -1,15 +1,32 @@
+require ('dotenv').config()
 const express = require('express');
 const { resolve } = require('path');
+const mongoose = require('mongoose');
 
 const app = express();
 const port = 3010;
 
 app.use(express.static('static'));
 
+
+const MONGO_URL = process.env.MONGO_URL;
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(MONGO_URL); // 
+        console.log('Connected to database');
+    } catch (error) {
+        console.error(`Error connecting to database: ${error.message}`);
+        process.exit(1);
+    }
+};
+
+connectDB();
+
 app.get('/', (req, res) => {
-  res.sendFile(resolve(__dirname, 'pages/index.html'));
+    res.sendFile(resolve(__dirname, 'pages/index.html'));
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+    console.log(`Example app listening at http://localhost:${port}`);
 });
